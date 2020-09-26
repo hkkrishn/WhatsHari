cf68a591bdbc451e70d71f19a55e0d977b191032
Harikrishnan <hkkrishn563@gmail.com>
2020-09-25 19:18:01 -0700

    initial setup

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+ export const firebaseConfig = {
+    apiKey: "AIzaSyBaM-LGugBPAe7e0QNM_RN6vkpsx0_6-5Q",
+    authDomain: "whatshari-f7f06.firebaseapp.com",
+    databaseURL: "https://whatshari-f7f06.firebaseio.com",
+    projectId: "whatshari-f7f06",
+    storageBucket: "whatshari-f7f06.appspot.com",
+    messagingSenderId: "112657459577",
+    appId: "1:112657459577:web:d4a1020c68cbc7d5f067a8"
+  };
