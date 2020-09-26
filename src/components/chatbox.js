0b0d93d389d8e38f8c8c014bf70199eb82a89a8f
Harikrishnan <hkkrishn563@gmail.com>
2020-09-26 12:13:40 -0700

    finished styling

--- src/components/chatbox.js
+++ src/components/chatbox.js
@@ -4,9 +4,10 @@
 //Description: Main Container for the sidebar component
 
 import React from 'react'
-import {Avatar, IconButton} from '@material-ui/core'
+import {Avatar,IconButton} from '@material-ui/core'
 import './chatbox.css'
-import { AttachFile, SearchOutlined,MoreVert } from '@material-ui/icons'
+import { AttachFile, SearchOutlined,MoreVert, InsertEmoticon } from '@material-ui/icons'
+import MicIcon from '@material-ui/icons/Mic'
 
 const Chatbox = () => {
   return (
@@ -28,6 +29,37 @@ const Chatbox = () => {
                 <MoreVert/>
             </IconButton>
         </div>
+    </div>
+    <div class="chat__body">
+        <p className = "chat__message">
+        <span class="chat__name">
+            Hari
+        </span>
+        Test message
+        <span class="chat__timestamp">
+            {new Date().toUTCString()}
+        </span>
+        </p>
+
+        <p className = "chat__message chat__receiver">
+        <span class="chat__name">
+            Dingle
+        </span>
+        Test message
+        <span class="chat__timestamp">
+            {new Date().toUTCString()}
+        </span>
+        </p>
+    </div>
+    <div class="chat__footer">
+        <InsertEmoticon/>
+        <form>
+            <input placeholder = "Type a message" type = "text"/>
+            <button type = "submit">Send a message</button>
+        </form>
+        <MicIcon/>
+
+
     </div>
 
     </div>
